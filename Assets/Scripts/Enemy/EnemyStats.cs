f6d388c66805da38f0ac669dfe53a3290c9f3bc5
Zoé <z.henriot@rubika-edu.com>
2025-10-02 14:44:05 +0200

    création script enemy stat + créaton colider knife

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,36 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour
+{ 
+    public EnemyScriptableObject enemyData;
+    
+    //Current stats
+    float currentMoveSpeed;
+    float currentHealth;
+    float currentDamage;
+
+    void Awake()
+    {
+        currentMoveSpeed  = enemyData.MoveSpeed;
+        currentHealth = enemyData.MaxHealth;
+        currentDamage = enemyData.Damage;
+    }
+
+    public void TakeDamage(float dmg)
+    {
+        
+        currentHealth -= dmg;
+
+        if (currentHealth <= 0)
+        {
+            kill();
+        }
+        
+    }
+
+    public void kill()
+    {
+        Destroy(gameObject);
+    }
+}
