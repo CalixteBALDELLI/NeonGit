cdb1e8f09867647d13c4c6a4cd71ea4528cf9f2f
LICATA Thomas <t.licata@rubika-edu.com>
2025-11-14 15:32:02 +0100

    Menu de la map
    On doit maintenir tab pour l'ouvrir, ca assombrit le fond mais ca mets pas en pause, il manque juste les scripts pour que l'icone change de place quand on se téléporte

--- Assets/Scripts/Menu/MapMenu.cs
+++ Assets/Scripts/Menu/MapMenu.cs
@@ -5,7 +5,13 @@ public class MapMenu : MonoBehaviour
     [SerializeField] GameObject mapMenuPanel;
     void Update()
     {
-        
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            mapMenuPanel.SetActive(true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            mapMenuPanel.SetActive(false);
+        }
     }
 }
