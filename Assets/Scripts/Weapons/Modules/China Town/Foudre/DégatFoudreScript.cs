e60e7a5200023e207750387b34303cacc72b5e9e
Zoé <z.henriot@rubika-edu.com>
2025-11-24 15:31:38 +0100

    Foudre90%

--- Assets/Scripts/Weapons/Modules/China Town/Foudre/DégatFoudreScript.cs
+++ Assets/Scripts/Weapons/Modules/China Town/Foudre/DégatFoudreScript.cs
@@ -1,12 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class DegatFoudre : MonoBehaviour
+public class DégatFoudreScript : MonoBehaviour
 {
-    void OnTriggerStay2D(Collider2D other)
+    public PlayerStats playerStats;            // Référence aux stats du joueur
+    public float foudreModuleDamages = 10;     // Diviseur des dégâts
+    public float speedDamage = 0.25f;         // Intervalle entre les dégâts
+    public float durationFoudre = 10f;  
+    public EnemyStat enemyStat;
+
+    private List<EnemyStat> enemiesInZone = new List<EnemyStat>();
+    private bool isDamaging = false;
+
+    void Start()
     {
-        Debug.Log(other.gameObject.name);
-        Destroy(other.gameObject);
-        
+        playerStats = PlayerStats.SINGLETON;
+        StartCoroutine(DestroyAfterDuration());
     }
-    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyStat enemy = other.GetComponent<EnemyStat>();
+            if (enemy != null && !enemiesInZone.Contains(enemy))
+            {
+                enemiesInZone.Add(enemy);
+
+                // Démarre la coroutine si ce n'est pas déjà fait
+                if (!isDamaging)
+                {
+                    isDamaging = true;
+                    StartCoroutine(DamageEnemies());
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyStat enemy = other.GetComponent<EnemyStat>();
+            if (enemy != null && enemiesInZone.Contains(enemy))
+            {
+                enemiesInZone.Remove(enemy);
+            }
+        }
+    }
+
+    IEnumerator DamageEnemies()
+    {
+        while (enemiesInZone.Count > 0)
+        {
+            foreach (EnemyStat enemy in enemiesInZone)
+            {
+                Debug.Log(enemy.name + " Before Damage: " + enemy.currentHealth);
+                enemy.currentHealth -= playerStats.currentPlayerDamage * foudreModuleDamages;
+                Debug.Log(enemy.name + " took damage. Health now: " + enemy.currentHealth);
+            }
+            yield return new WaitForSeconds(speedDamage);
+        }
+        isDamaging = false;
+    }
+
+    IEnumerator DestroyAfterDuration()
+    {
+        yield return new WaitForSeconds(durationFoudre);
+        Destroy(gameObject);
+    }
 }
